a1f84ece61d064afb993f4200f0665a385a9191f
radiyakhan <radiyakhan59@gmail.com>
2024-08-18 22:54:54 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoquestion = await inquirer.prompt([
+        {
+            name: "firstquestion",
+            type: "input",
+            message: "What would you like to add in your todos?",
+        },
+        {
+            name: "secondquestion",
+            type: "confirm",
+            message: "Would you like to add more in your todos?",
+            default: "true",
+        },
+    ]);
+    todos.push(todoquestion.firstquestion);
+    console.log(todos);
+    condition = todoquestion.secondquestion;
+}
